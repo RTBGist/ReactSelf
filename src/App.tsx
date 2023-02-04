783c51bae60653a6b0b3437ce8071fc83cfa4f96
Yourdeveloper <Yourdeveloper@mail.ru>
2023-02-04 14:47:52 +0700

    Добавил react router dom, добавили чанки для разбивания страничек main/about, добавил suspense/lazy для подгрузки нужных страниц, а не всего бандла разом

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import './index.scss';
+import {Link, Route, Routes} from "react-router-dom";
+
+import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
+import {MainPageAsync} from "./pages/MainPage/MainPage.async";
 
 
 const App = () => {
 	return (
 			<div className='app'>
-				<div className="test">123</div>
+
+				<Link to={'/'}>Главная</Link>
+				<Link to={'/about'}>О сайте</Link>
+
+				<Suspense fallback={<div>Loading...</div>}>
+					<Routes>
+						<Route path={'/about'} element={<AboutPageAsync />} />
+						<Route path={'/'} element={<MainPageAsync />} />
+					</Routes>
+				</Suspense>
+
 			</div>
 	);
 };
