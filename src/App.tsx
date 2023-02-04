bc3672047c7a37e9981597cb12e801aa0a97def6
Yourdeveloper <Yourdeveloper@mail.ru>
2023-02-04 15:24:18 +0700

    Организация стилей(переменные, темы), Создание контекста Темы и сохранение темы в Local Storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,19 @@
-import React, { Suspense } from 'react';
-import './index.scss';
+import React, {Suspense} from 'react';
+import './styles/index.scss';
 import {Link, Route, Routes} from "react-router-dom";
 
 import {AboutPageAsync} from "./pages/AboutPage/AboutPage.async";
 import {MainPageAsync} from "./pages/MainPage/MainPage.async";
+import {useTheme} from "./theme/useTheme";
 
 
 const App = () => {
+	const {theme, toggleTheme} = useTheme();
+
 	return (
-			<div className='app'>
+			<div className={`app ${theme}`}>
+
+				<button onClick={toggleTheme}>Toggle theme</button>
 
 				<Link to={'/'}>Главная</Link>
 				<Link to={'/about'}>О сайте</Link>
