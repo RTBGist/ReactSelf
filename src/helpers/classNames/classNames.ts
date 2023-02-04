1a73efc31d898f28a4587e9ef89a393b0ee7206d
Yourdeveloper <Yourdeveloper@mail.ru>
2023-02-04 18:17:52 +0700

    Добавил хелпер - className

---
+++ src/helpers/classNames/classNames.ts
@@ -0,0 +1,13 @@
+type Mods = Record<string, boolean | string> // object { string: boolean | string }
+
+export function classNames(cls: string, mods: Mods, additional: string[]): string {
+	return [
+			cls,
+			...additional,
+			...Object.entries(mods)
+					.filter(([className, value]) => Boolean(value)) // вернули только значение, где value === true
+					.map(([className, value]) => className) // забрали только классы, флаги выкинули
+	]
+			.join(' ');
+}
+
