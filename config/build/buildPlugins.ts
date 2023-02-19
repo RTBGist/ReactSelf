6f0ba94aec02924ef9cf443d8856ce3057d2a21d
Yourdeveloper <Yourdeveloper@mail.ru>
2023-02-20 05:57:46 +0700

    Фикс buildPlugins lint ts, Фикс screenshot test approve

--- config/build/buildPlugins.ts
+++ config/build/buildPlugins.ts
@@ -5,31 +5,31 @@ import { BundleAnalyzerPlugin } from 'webpack-bundle-analyzer';
 import { BuildOptions } from './types/config';
 
 export function buildPlugins({ paths, isDev }: BuildOptions): webpack.WebpackPluginInstance[] {
-	const plugins = [
-		// создает html файл
-		new HtmlWebpackPlugin({
-			// используем наш html файл как шаблон
-			template: paths.html,
-		}),
-		// показывает прогресс сборки
-		new webpack.ProgressPlugin(),
-		// Создает css файлы отдельные, то есть не в общем бандлее будут
-		new MiniCssExtractPlugin({
-			filename: 'css/[name].[contenthash:8].css',
-			chunkFilename: 'css/[name].[contenthash:8].css',
-		}),
-		new webpack.DefinePlugin({
-			// Прокинули глобальную переменную в проект
-			__IS_DEV__: JSON.stringify(isDev),
-		})
-	];
+  const plugins = [
+    // создает html файл
+    new HtmlWebpackPlugin({
+      // используем наш html файл как шаблон
+      template: paths.html,
+    }),
+    // показывает прогресс сборки
+    new webpack.ProgressPlugin(),
+    // Создает css файлы отдельные, то есть не в общем бандлее будут
+    new MiniCssExtractPlugin({
+      filename: 'css/[name].[contenthash:8].css',
+      chunkFilename: 'css/[name].[contenthash:8].css',
+    }),
+    new webpack.DefinePlugin({
+      // Прокинули глобальную переменную в проект
+      __IS_DEV__: JSON.stringify(isDev),
+    }),
+  ];
 
-	if (isDev) {
-		plugins.push(new BundleAnalyzerPlugin({
-			openAnalyzer: false,
-			analyzerMode: 'disabled', // отключен, чтоб не грузить сборку
-		}))
-	}
+  if (isDev) {
+    plugins.push(new BundleAnalyzerPlugin({
+      openAnalyzer: false,
+      analyzerMode: 'disabled', // отключен, чтоб не грузить сборку
+    }));
+  }
 
   return plugins;
 }
